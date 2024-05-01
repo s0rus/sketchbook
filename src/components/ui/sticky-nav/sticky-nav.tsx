0865688757955d675906d0e816c8428b73c83ed3
soruse <piotrmolkontakt@gmail.com>
2024-05-01 19:58:22 +0200

    fix(component-scroller): 🪱 skeleton for suspense

--- src/components/ui/sticky-nav/sticky-nav.tsx
+++ src/components/ui/sticky-nav/sticky-nav.tsx
@@ -1,4 +1,5 @@
 import { Suspense } from "react";
+import { Skeleton } from "../skeleton";
 import { ThemeToggle } from "../theme-toggle";
 import { ComponentScroller } from "./component-scroller";
 import { ScrollToTopButton } from "./scroll-to-top-button";
@@ -9,7 +10,7 @@ export function StickyNav(props: { componentCount: number }) {
       <div className="transfrom fixed bottom-10 left-0 z-50 flex w-full items-center justify-center md:sticky md:top-10">
         <nav className="flex flex-row gap-2 rounded-full border border-foreground/5 bg-card p-2 md:flex-col">
           <ThemeToggle />
-          <Suspense>
+          <Suspense fallback={<Skeleton className="h-6 w-6 rounded-full"></Skeleton>}>
             <ComponentScroller componentCount={props.componentCount} />
           </Suspense>
           <ScrollToTopButton />
