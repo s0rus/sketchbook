df0237f38a58c066995b7c37e60d9dbfe66a68d9
soruse <piotrmolkontakt@gmail.com>
2024-05-04 23:17:51 +0200

    feat(sketch):  🪄 dynamic island complete

--- src/components/sketches/dynamic-island.tsx
+++ src/components/sketches/dynamic-island.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { AnimatePresence, motion } from "framer-motion";
+import songAsset from "@/assets/dynamic-island-a1.jpg";
+import { AnimatePresence, MotionConfig, motion, type Variants } from "framer-motion";
+import Image from "next/image";
 import { useEffect, useMemo, useState } from "react";
 import { Button } from "../ui/button";
 import { Icon } from "../ui/icon";
@@ -18,11 +20,23 @@ export function DynamicIsland() {
   const [islandState, setIslandState] = useState<IslandState>("IDLE");
   const [isTimerRunning, setIsTimerRunning] = useState(true);
   const [time, setTime] = useState(0);
+  const [isSilentMode, setIsSilentMode] = useState(false);
+
+  const progressBarX = 100 - (time / 222) * 100;
+  const timer = useMemo(() => formatTimer(time), [time]);
 
   useEffect(() => {
     let timer: NodeJS.Timeout;
 
     if (isTimerRunning) {
+      if (islandState === "SONG" && time > 222) {
+        setTime(0);
+      }
+
+      if (islandState === "RING" && time % 2 === 0 && time > 0) {
+        setIsSilentMode((prevSilent) => !prevSilent);
+      }
+
       if (time > 0) {
         timer = setInterval(() => {
           setTime((prevTime) => prevTime + 1);
@@ -38,7 +52,7 @@ export function DynamicIsland() {
       clearInterval(timer);
       clearTimeout(timer);
     };
-  }, [isTimerRunning, time]);
+  }, [isTimerRunning, time, islandState]);
 
   const content = useMemo(() => {
     switch (islandState) {
@@ -48,7 +62,7 @@ export function DynamicIsland() {
             key="IDLE"
             layoutId="island"
             className="h-[28px] min-w-[100px] bg-black"
-            style={{ borderRadius: "9999px" }}
+            style={{ borderRadius: "32px" }}
           ></motion.div>
         );
       case "RING":
@@ -56,8 +70,8 @@ export function DynamicIsland() {
           <motion.div
             key="RING"
             layoutId="island"
-            className="flex w-[120px] items-center justify-between bg-black px-3 py-1"
-            style={{ borderRadius: "9999px" }}
+            className="flex items-center justify-between gap-16 bg-black px-2 py-1"
+            style={{ borderRadius: "32px" }}
           >
             <motion.div
               initial={{
@@ -80,8 +94,17 @@ export function DynamicIsland() {
                 duration: 0.4,
                 bounce: 0,
               }}
+              key={`ring-icon-${isSilentMode}`}
             >
-              <Icon.bell className="h-4 w-4" />
+              {isSilentMode ? (
+                <motion.span layoutId="ring-icon" className="block w-full rounded-full bg-red-500 px-3 py-0.5 text-white">
+                  <Icon.bellOff className="h-[14px] w-[14px]" />
+                </motion.span>
+              ) : (
+                <motion.span layoutId="ring-icon">
+                  <Icon.bell className="h-[14px] w-[14px]" />
+                </motion.span>
+              )}
             </motion.div>
             <motion.p
               initial={{
@@ -105,8 +128,15 @@ export function DynamicIsland() {
                 bounce: 0,
               }}
               className="text-sm font-bold"
+              key={`ring-text-${isSilentMode}`}
             >
-              Ring
+              {isSilentMode ? (
+                <motion.span className="text-red-500" layoutId="ring-text">
+                  Silent
+                </motion.span>
+              ) : (
+                <motion.span layoutId="ring-text">Ring</motion.span>
+              )}
             </motion.p>
           </motion.div>
         );
@@ -116,21 +146,19 @@ export function DynamicIsland() {
             key="TIMER"
             layoutId="island"
             className="flex items-center justify-between gap-6 bg-black px-4 py-3"
-            style={{ borderRadius: "9999px" }}
+            style={{ borderRadius: "32px" }}
+            initial={{ filter: "blur(8px)", opacity: 0 }}
+            animate={{
+              opacity: 1,
+              filter: "blur(0px)",
+            }}
+            transition={{
+              type: "spring",
+              duration: 0.4,
+              bounce: 0,
+            }}
           >
-            <motion.div
-              initial={{ filter: "blur(4px)", opacity: 0 }}
-              animate={{
-                opacity: 1,
-                filter: "blur(0px)",
-              }}
-              transition={{
-                type: "spring",
-                duration: 0.6,
-                bounce: 0,
-              }}
-              className="flex flex-row items-center gap-1"
-            >
+            <div className="flex flex-row items-center gap-1">
               <AnimatePresence mode="wait" initial={false}>
                 <button
                   className="flex items-center justify-center rounded-full bg-primary/20 p-3 text-primary transition-colors hover:bg-primary/40"
@@ -157,22 +185,155 @@ export function DynamicIsland() {
               >
                 <Icon.X className="h-5 w-5" />
               </button>
-            </motion.div>
+            </div>
             <div className="mt-2 flex items-baseline gap-1 text-primary">
               <span className="text-sm font-bold">Timer</span>
-              <span className="font-mono text-3xl">{`${String(Math.floor(time / 60)).padStart(2, "0")}:${String(time % 60).padStart(2, "0")}`}</span>
+              <p>
+                <MotionConfig
+                  transition={{
+                    type: "spring",
+                    duration: 0.6,
+                    bounce: 0,
+                  }}
+                >
+                  <AnimatePresence mode="popLayout">
+                    <motion.span
+                      className="font-mono text-3xl"
+                      key={`timerMinutesFirstDigit-${timer.minutesFirstDigit}`}
+                      variants={timerVariants}
+                      initial="initial"
+                      animate="visible"
+                      exit="hidden"
+                    >
+                      {timer.minutesFirstDigit}
+                    </motion.span>
+                    <motion.span
+                      className="font-mono text-3xl"
+                      key={`timerMinutesSecondDigit-${timer.minutesFirstDigit}`}
+                      variants={timerVariants}
+                      initial="initial"
+                      animate="visible"
+                      exit="hidden"
+                    >
+                      {timer.minutesSecondDigit}
+                    </motion.span>
+                    <span className="font-mono text-3xl">:</span>
+                    <motion.span
+                      className="font-mono text-3xl"
+                      key={`timerSecondsSecondDigit-${timer.secondsFirstDigit}`}
+                      variants={timerVariants}
+                      initial="initial"
+                      animate="visible"
+                      exit="hidden"
+                    >
+                      {timer.secondsFirstDigit}
+                    </motion.span>
+                    <motion.span
+                      className="font-mono text-3xl"
+                      key={`timerSecondsDigit-${timer.secondsSecondDigit}`}
+                      variants={timerVariants}
+                      initial="initial"
+                      animate="visible"
+                      exit="hidden"
+                    >
+                      {timer.secondsSecondDigit}
+                    </motion.span>
+                  </AnimatePresence>
+                </MotionConfig>
+              </p>
             </div>
           </motion.div>
         );
       case "SONG":
-        <div>xd</div>;
+        return (
+          <motion.div
+            key="SONG"
+            layoutId="island"
+            className="flex items-center justify-between gap-6 bg-black px-8 py-6"
+            style={{ borderRadius: "32px" }}
+            initial={{ filter: "blur(8px)", opacity: 0 }}
+            animate={{
+              opacity: 1,
+              filter: "blur(0px)",
+            }}
+            transition={{
+              type: "spring",
+              duration: 0.4,
+              bounce: 0,
+            }}
+          >
+            <div className="flex flex-col gap-2">
+              <div className="flex flex-row items-center justify-between gap-12">
+                <div className="flex flex-row items-center gap-2">
+                  <Image src={songAsset} alt="song" width={48} height={48} className="aspect-square h-[48px] w-[48px] rounded-md" />
+                  <div>
+                    <p className="text-sm font-bold">imgonnagetyouback</p>
+                    <p className="text-sm leading-3 text-white/60">Taylor Swift</p>
+                  </div>
+                </div>
+                <div className="flex flex-row items-center gap-0.5">
+                  <AnimatePresence mode="popLayout">
+                    {Array.from({ length: 10 }).map((_, index) =>
+                      isTimerRunning ? (
+                        <motion.div
+                          key={`soundbar-${index}`}
+                          animate={"visible"}
+                          variants={songVariants}
+                          custom={index + 1}
+                          className="w-0.5 bg-primary"
+                        ></motion.div>
+                      ) : (
+                        <motion.div key={`soundbar-${index}`} animate={{ height: "1px" }} className="w-0.5 bg-primary"></motion.div>
+                      ),
+                    )}
+                  </AnimatePresence>
+                </div>
+              </div>
+              <div className="flex flex-row items-center gap-2">
+                <span className="w-[36px] text-left text-sm text-white/60">{`${formatTime(time)}`}</span>
+                <div className="relative h-0.5 flex-1 overflow-hidden rounded-sm bg-muted">
+                  <motion.div
+                    className="absolute inset-0 bg-white"
+                    initial={{ x: "-100%" }}
+                    animate={{ x: `-${progressBarX}%` }}
+                    transition={{
+                      type: "spring",
+                      duration: 0.1,
+                      bounce: 0,
+                    }}
+                  ></motion.div>
+                </div>
+                <span className="min-w-[36px] text-right text-sm text-white/60">3:42</span>
+              </div>
+              <div className="mt-1 flex flex-row items-center justify-center gap-4 pb-1">
+                <Icon.skipBack className="h-5 w-5" />
+                <AnimatePresence mode="wait" initial={false}>
+                  <button className="flex items-center justify-center" onClick={() => setIsTimerRunning((prevRunning) => !prevRunning)}>
+                    {isTimerRunning ? (
+                      <motion.span key="pause" variants={variants} initial="hidden" animate="visible" exit="hidden">
+                        <Icon.pause className="h-6 w-6" />
+                      </motion.span>
+                    ) : (
+                      <motion.span key="play" variants={variants} initial="hidden" animate="visible" exit="hidden">
+                        <Icon.play className="h-6 w-6" />
+                      </motion.span>
+                    )}
+                  </button>
+                </AnimatePresence>
+                <Icon.skipForward className="h-5 w-5" />
+              </div>
+            </div>
+          </motion.div>
+        );
     }
-  }, [islandState, isTimerRunning, time]);
+  }, [islandState, isTimerRunning, time, progressBarX, timer, isSilentMode]);
 
   return (
     <div className="flex flex-col items-center gap-y-4">
-      <div className="min-h-[150px]">
-        <AnimatePresence mode="popLayout">{content}</AnimatePresence>
+      <div className="min-h-[200px]">
+        <AnimatePresence mode="popLayout" initial={false}>
+          {content}
+        </AnimatePresence>
       </div>
       <div className="flex flex-row gap-x-2">
         {Object.values(ISLAND_STATES).map((state) => (
@@ -183,6 +344,7 @@ export function DynamicIsland() {
             onClick={() => {
               setIsTimerRunning(true);
               setTime(0);
+              setIsSilentMode(false);
               setIslandState(state);
             }}
           >
@@ -194,7 +356,51 @@ export function DynamicIsland() {
   );
 }
 
-const variants = {
+const timerVariants: Variants = {
+  initial: {
+    y: 24,
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+  hidden: {
+    y: -12,
+    opacity: 0,
+  },
+};
+
+const variants: Variants = {
   hidden: { opacity: 0, scale: 0.5 },
   visible: { opacity: 1, scale: 1 },
 };
+
+const songVariants: Variants = {
+  visible: (index: number) => ({
+    height: [`4px`, `${4 + 24 * (index / 10)}px`, `4px`],
+    transition: {
+      repeat: Infinity,
+      duration: Math.random() + index / 10,
+      ease: "easeOut",
+    },
+  }),
+  hidden: { height: ["1px"] },
+};
+
+function formatTime(timeInSeconds: number) {
+  const minutes = Math.floor(timeInSeconds / 60);
+  const seconds = timeInSeconds % 60;
+  return `${String(minutes).padStart(1, "0")}:${String(seconds).padStart(2, "0")}`;
+}
+
+function formatTimer(timeInSeconds: number) {
+  const minutes = Math.floor(timeInSeconds / 60);
+  const seconds = timeInSeconds % 60;
+  return {
+    minutesFirstDigit: Math.floor(minutes / 10),
+    minutesSecondDigit: minutes % 10,
+    secondsFirstDigit: Math.floor(seconds / 10),
+    secondsSecondDigit: seconds % 10,
+  };
+}
--- src/components/ui/icon.tsx
+++ src/components/ui/icon.tsx
@@ -1,4 +1,4 @@
-import { ArrowUp, Bell, CheckCircle, Loader2, Moon, Notebook, Sun, X, type LucideProps } from "lucide-react";
+import { ArrowUp, Bell, BellOff, CheckCircle, Loader2, Moon, Notebook, Sun, X, type LucideProps } from "lucide-react";
 
 export const Icon = {
   sun: Sun,
@@ -8,6 +8,7 @@ export const Icon = {
   spinner: Loader2,
   checkCircle: CheckCircle,
   bell: Bell,
+  bellOff: BellOff,
   X: X,
   play: (props: LucideProps) => (
     <svg height="16" strokeLinejoin="round" viewBox="0 0 16 16" width="16" {...props}>
@@ -25,6 +26,26 @@ export const Icon = {
         fillRule="evenodd"
         clipRule="evenodd"
         d="M5.5 2.5V1.75H4V2.5V13.5V14.25H5.5V13.5V2.5ZM12 2.5V1.75H10.5V2.5V13.5V14.25H12V13.5V2.5Z"
+        fill="currentColor"
+      ></path>
+    </svg>
+  ),
+  skipBack: (props: LucideProps) => (
+    <svg height="16" strokeLinejoin="round" viewBox="0 0 16 16" width="16" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M4.08144 8.21092C3.92706 8.11268 3.92706 7.88733 4.08144 7.78909L14.3658 1.24451C14.5322 1.1386 14.75 1.25815 14.75 1.45542L14.75 14.5446C14.75 14.7419 14.5322 14.8614 14.3658 14.7555L4.08144 8.21092ZM0.75 2V1.25H2.25V2V14V14.75H0.75V14V2Z"
+        fill="currentColor"
+      ></path>
+    </svg>
+  ),
+  skipForward: (props: LucideProps) => (
+    <svg height="16" strokeLinejoin="round" viewBox="0 0 16 16" width="16" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M11.6686 8.21092C11.8229 8.11268 11.8229 7.88733 11.6686 7.78909L1.38422 1.24451C1.21779 1.1386 1 1.25815 1 1.45542V14.5446C1 14.7419 1.21779 14.8614 1.38422 14.7555L11.6686 8.21092ZM15 2V1.25H13.5V2V14V14.75H15V14V2Z"
         fill="currentColor"
       ></path>
     </svg>
